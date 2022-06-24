c79cf78572df124adfdeb3621456a5c6cfbe9650
ozknkrm <kerimozkan11@gmail.com>
2022-06-24 22:50:48 +0200

    another task done

--- Micro_ejercicios/app.js
+++ Micro_ejercicios/app.js
@@ -3,15 +3,24 @@ let title = document.getElementById('Hello');
 
 let myFrase = 'Hello World';
 
-function printMyFrase(element){
+function printResults(element){
     title.innerHTML = element
 }
-
-printMyFrase(myFrase)
 
 function showMyCalculation(num1,num2) {
     num1 = 3
     num2 = 5
     return num1+num2
 }
-printMyFrase(showMyCalculation()) 
+
+function sayHi() {
+    let = person = prompt('PLEASE ENTER YOUR NAME', 'Kerim Ozkan');
+    if(person != null) {
+        document.getElementById('demo').innerHTML = 'Hello ' + person + '! How are you today?';
+    }
+
+
+}
+
+
+printResults(showMyCalculation())
